e5d137101b0e6726e04d759d54b737862cdfd930
huaxin <308668137@qq.com>
2020-04-15 16:52:24 +0800

    demos

---
+++ precache-manifest.fd665465cd8ae08e08aabdc17f95955e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cf58c26da5ed283fc530812887462da",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ff72cbc36d058c22f7f5",
+    "url": "./static/css/addDress.3332bfa4.chunk.css"
+  },
+  {
+    "revision": "32bfe6fe06421ba3ca8a",
+    "url": "./static/css/main.0407c4ea.chunk.css"
+  },
+  {
+    "revision": "659d14152025f31ffc8b",
+    "url": "./static/css/vendors.fa97f8e6.chunk.css"
+  },
+  {
+    "revision": "ff72cbc36d058c22f7f5",
+    "url": "./static/js/addDress.0e5c3d4f.chunk.js"
+  },
+  {
+    "revision": "32bfe6fe06421ba3ca8a",
+    "url": "./static/js/main.07d9a465.chunk.js"
+  },
+  {
+    "revision": "b6d2217bc7840abec50f",
+    "url": "./static/js/runtime-main.713c77fa.js"
+  },
+  {
+    "revision": "659d14152025f31ffc8b",
+    "url": "./static/js/vendors.f979c078.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "./static/js/vendors.f979c078.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5825f033c6ff12cd1ed1f3c99dff5e4b",
+    "url": "./static/media/background.5825f033.svg"
+  }
+]);
--- src/utils/loadable.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Loadable from 'react-loadable';
-import Loading from '../components/Loading';
-
-export default (loader) => Loadable({
-    loader,
-    loading: () => <Loading />
-});
